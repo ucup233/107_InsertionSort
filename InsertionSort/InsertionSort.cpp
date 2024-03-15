a121fbadeb143348f06472fcef7f541fb147eed0
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-03-15 10:44:34 +0700

    Insertion Sort Function

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -4,6 +4,13 @@ using namespace std;
 void insertionSort() {
 	int i, n, temp, j;
 	int arr[11];
-
-
+	for (i = 1; i <= n - 1; i++); {
+		temp = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j] > temp); {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = temp;
+	}
 }
