c683159b3be737c632b04f3af57869828bb4dfff
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-03-15 10:58:55 +0700

    Input function

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int i, temp, n, j;
+int arr[11];
+
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 11)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 11 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================";
+	cout << "Masukkan Elemen Array";
+	cout << "======================";
+}
+
 void insertionSort() {
-	int i, n, temp, j;
-	int arr[11];
 	for (i = 1; i <= n - 1; i++); {
 		temp = arr[i];
 		j = i - 1;
@@ -14,3 +31,4 @@ void insertionSort() {
 		arr[j + 1] = temp;
 	}
 }
+ 
