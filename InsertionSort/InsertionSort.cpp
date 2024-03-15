bc7ae43ba7cddbc084e702da0402fe0c49a01e85
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-03-15 11:02:58 +0700

    Display Function

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -32,3 +32,15 @@ void insertionSort() {
 	}
 }
  
+void display() {
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "===================" << endl;
+	for (int j = 0; j < n; j++); {
+		cout << arr[j] << endl;
+	}
+	cout << "Jumlah Pass = " << n - 1 << endl;
+	cout << endl;
+}
+
